e3d521f9dee7d538141d511cdad0e1c2cd465133
Ravikant Singh <122196249+raavikant27@users.noreply.github.com>
2025-04-23 11:54:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim leading and trailing spaces
+  str = str.trim();
+
+  // If the string is empty after trimming, return it
+  if (str === "") {
+    return "";
+  }
+
+  // Find the index of the first space
+  const spaceIndex = str.indexOf(" ");
+
+  // If no space is found, return the whole string
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  // Otherwise, return the substring up to the first space
+  return str.substring(0, spaceIndex);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
